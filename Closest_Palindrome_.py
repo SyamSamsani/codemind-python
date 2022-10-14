ca754ab2cd65a2b5b4c9360cba6de34533128f44
SyamSamsani <109623822+SyamSamsani@users.noreply.github.com>
2022-10-14 20:01:18 +0530

    def palindrome(n):     s = 0     while n:         s = s*10 + n%10         n = n//10     return s n = int(input()) for i in range(n+1,10*n):     if (i==palindrome(i)):         fp = i         break for i in range(n-1,1,-1):     if (i==palindrome(i)):         sp = i         break if ((fp-n)>(n-sp)):     print(sp) elif ((fp-n)<(n-sp)):     print(fp) else:     print(sp,fp)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,22 +1,22 @@
-def pali(n):
-    s=0
-    while(n):
-        r=n%10
-        s=s*10+r
-        n=n//10
+def palindrome(n):
+    s = 0
+    while n:
+        s = s*10 + n%10
+        n = n//10
     return s
-n=int(input())
+n = int(input())
 for i in range(n+1,10*n):
-    if(i==pali(i)):
-        fp=i
+    if (i==palindrome(i)):
+        fp = i
         break
 for i in range(n-1,1,-1):
-    if(i==pali(i)):
-        sp=i
+    if (i==palindrome(i)):
+        sp = i
         break
-if((fp-n)>(n-sp)):
+if ((fp-n)>(n-sp)):
     print(sp)
-elif((fp-n)<(n-sp)):
+elif ((fp-n)<(n-sp)):
     print(fp)
 else:
     print(sp,fp)
+    
